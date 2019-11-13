d84bc368903e8f18e182f5c4c1ce4d9866923fb7
Gregory <gregoryinouye@gmail.com>
2019-11-13 14:49:21 -0800

    add Jest and Enzyme test suite

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: ["babel-loader"],
       },
     ],
   },
