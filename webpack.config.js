32b490293263ff0e258e8855e31b866ebac19cda
Gregory <gregoryinouye@gmail.com>
2019-11-13 14:52:32 -0800

    fix webpack config to autoresolve .js and .jsx file extensions

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,11 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+        loader: "babel-loader"
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
